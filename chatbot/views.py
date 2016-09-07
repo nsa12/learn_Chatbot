2de7415b926b9c40facd80a829b1b51f74a8807f
Saurav Tomar <stomatrix@gmail.com>
2016-09-07 17:15:08 +0530

    first commit

---
+++ chatbot/views.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+# Create your views here.
+
+VERIFY_TOKEN = '7thseptember2016'
+
+class MyChatBotView(generic.View):
+	def get (self, request, *args, **kwargs):
+		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+			return HttpResponse(self.request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Oops invalid token')
+
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return generic.View.dispatch(self, request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		incoming_mesage= json.loads(self.request.body.decode('utf-8'))
+		print incoming_mesage
+
+def index(request):
+	return HttpResponse('Hello world')
